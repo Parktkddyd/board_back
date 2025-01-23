16c2e912b0b14edd311ca40efc3273483c8c4d91
Parktkddyd <s_in_b_u@naver.com>
2025-01-23 18:26:59 +0900

    feat: 유저의 세션을 통해 아이디를 반환하도록 구현

---
+++ src/main/java/com/syp/board_back/dto/user/response/session/SessionResponse.java
@@ -0,0 +1,10 @@
+package com.syp.board_back.dto.user.response.session;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SessionResponse {
+    private String user_id;
+}
--- src/main/java/com/syp/board_back/service/user/SessionService.java
+++ src/main/java/com/syp/board_back/service/user/SessionService.java
@@ -1,6 +1,9 @@
 package com.syp.board_back.service.user;
 
+import com.syp.board_back.common.constant.SessionConst;
+import com.syp.board_back.domain.user.User;
 import com.syp.board_back.dto.user.response.login.LoginResponse;
+import com.syp.board_back.dto.user.response.session.SessionResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,12 @@ public class SessionService {
         HttpSession session = servletReq.getSession(false);
 
         return new LoginResponse(session.getId());
+    }
+
+    public SessionResponse getUserIdBySession(HttpServletRequest servletReq) {
+        HttpSession session = servletReq.getSession(false);
+        User user = (User) session.getAttribute(SessionConst.LOGIN_USER);
+        String user_id = user.getUser_id();
+        return new SessionResponse(user_id);
     }
 }
